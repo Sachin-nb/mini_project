8eedb1f303f23b6ac4e61ffc06b2ad4857b1f1a5
Sachin-nb <sachinn@kssem.edu.in>
2026-04-11 23:33:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1833,3 +1833,7 @@ def reply_message(msg_id):
 # -----------------------------
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
